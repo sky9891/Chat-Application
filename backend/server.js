a1e43688f885cba08f49c5c90adef771f3f512cf
sky9891 <akash.rai@iitg.ac.in>
2025-09-04 11:46:19 +0530

    commit

---
+++ backend/server.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const colors = require("colors"); // ✅ make console.log colored
+
+// Load env vars
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// API Routes
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+// -------------------------- Deployment ------------------------------
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// -------------------------- Deployment ------------------------------
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+// PORT fallback
+const PORT = process.env.PORT || 5000;
+
+// Start server
+const server = app.listen(PORT, () =>
+  console.log(`🚀 Server running on PORT ${PORT}...`.yellow.bold)
+);
+
+// -------------------------- Socket.io ------------------------------
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("⚡ Connected to socket.io".cyan.bold);
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log(`📥 User joined room: ${room}`.green);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    const chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id === newMessageRecieved.sender._id) return;
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("❌ USER DISCONNECTED".red.bold);
+    socket.leave(socket.userData?._id);
+  });
+});
+// -------------------------- Socket.io ------------------------------
